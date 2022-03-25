f3755dd0350445b6ccd3aa09f6e573fb908e4b7e
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-25 17:59:45 -0500

    Configuración del componente Link que así como NavLink no refresca al hacer click sobre el

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,16 @@
+import {Link} from 'react-router-dom';
+
+const userId = 10
+
 export default function HomePage() {
   return (
-    <div><h1>Hello world</h1></div>
+    <div>
+      <h1>Application</h1>
+
+      <Link to={'/users/${userId}'}>User Error</Link>
+      <br></br>
+      <Link to="/users">Users</Link>
+    </div>
   )
 }
 
