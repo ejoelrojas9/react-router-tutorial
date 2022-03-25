f5e6823bd9a7c5f9b381e13defaf674439ca2ee0
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-25 16:43:47 -0500

    Se configuró la ruta para el error 404 Not Found, es decir se aplicó la configuracion para todas aquellas rutas que no estan declaradas den error 404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import UserPage from './pages/UserPage'
+import NotFoundPage from "./pages/NotFoundPage";
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/about" element={<AboutPage/>}/>
         <Route path="/users" element={<UserPage/>}/>
+        <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
     
     </BrowserRouter>
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,6 @@
+export default function NotFoundPage() {
+  return (
+    <div><h1>404 
+    Not Found</h1></div>
+  )
+}
