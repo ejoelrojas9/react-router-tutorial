19ebba4af8dd52140c93161fa6c744cb2d0ca59f
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-26 10:37:40 -0500

    Configuración de params, como importar la libreria y tambien como hacer para que el string que se coloque en la barra de explorador se tome por el codigo con el metodo params

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import UsersPage from './pages/UsersPage';
 import UserPage from './pages/UserPage'
 import NotFoundPage from "./pages/NotFoundPage";
 import Navbar from "./components/Navbar";
@@ -15,7 +16,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/about" element={<AboutPage/>}/>
-        <Route path="/users" element={<UserPage/>}/>
+        <Route path="/users" element={<UsersPage/>}/>
+        <Route path="/users/:id" element={<UserPage/>}/>
         <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
     
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import {useParams} from 'react-router-dom'
 
-export default function UserPage() {
-    return ( <
-        div > < h1 > UserPage < /h1></div >
-    )
+export default function UserPage () {
+  //const params = useParams()
+  // console.log(params);
+  const {id} = useParams()
+
+  return (
+    //<div>User <h1>{params.id}</h1> </div>
+    <div>User <h1>{id}</h1> </div>
+  )
 }
