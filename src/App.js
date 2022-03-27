0bf43ccd971dbb9374f6d6f669a710de3e808af8
Joel Rojas <ejoelrojas9@gmail.com>
2022-03-27 05:39:34 -0500

    Configuración de useNAvigate como boton para hacer una acción, en este caso ir a la pagina principal

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import UsersPage from './pages/UsersPage';
 import UserPage from './pages/UserPage'
 import NotFoundPage from "./pages/NotFoundPage";
 import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
 
 
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/users" element={<UsersPage/>}/>
         <Route path="/usuarios" element={<Navigate to="/users"/>} />
         <Route path="/users/:id" element={<UserPage/>}/>
+        <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
     
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,18 @@
+import {useNavigate} from 'react-router-dom'
+
+export default function Dashboard() {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate("/");
+  };
+    return ( <
+        div > 
+          < h1 > Dashboard < /h1>
+        <button onClick={handleClick}>
+        Logout
+        </button>
+        </div >
+
+    )
+}
